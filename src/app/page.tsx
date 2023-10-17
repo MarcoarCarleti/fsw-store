804678fc49cfdf68e90299e21549a7a6a479ff45
MarcoarCarleti <marcarleti@gmail.com>
2023-10-16 21:21:19 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
 
 export default function Home() {
-  return <div className=" text-red-500">
-    
-  </div>;
+  return <div className=" text-red-500">Hello World</div>;
 }
