4775376335ac7909f1709da01e377bf3df15a6c9
MarcoarCarleti <marcarleti@gmail.com>
2023-10-18 01:04:55 -0300

    feat: ensure user goes to catalog page on catalog button click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -10,10 +10,17 @@ import {
 } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import { Separator } from "./separator";
+import Link from "next/link";
 
 export function Header() {
   const { status, data } = useSession();
@@ -65,27 +72,41 @@ export function Header() {
                   <div className="flex flex-col">
                     <p className="font-medium ">{data.user.name}</p>
                     <p className="text-sm opacity-75">Boas Compras!</p>
-                  </div >
+                  </div>
                 </div>
 
                 <Separator />
               </div>
             )}
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <HomeIcon size={16} />
-              Início
-            </Button>
+            <SheetClose asChild>
+              <Link href={"/"}>
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <HomeIcon size={16} />
+                  Início
+                </Button>
+              </Link>
+            </SheetClose>
 
             <Button variant="outline" className="w-full justify-start gap-2">
               <PercentIcon size={16} />
               Ofertas
             </Button>
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <ListOrderedIcon size={16} />
-              Catalogo
-            </Button>
+            <SheetClose asChild>
+              <Link href={"/catalog"}>
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <ListOrderedIcon size={16} />
+                  Catalogo
+                </Button>
+              </Link>
+            </SheetClose>
 
             {status === "authenticated" && (
               <Button
@@ -101,9 +122,11 @@ export function Header() {
         </SheetContent>
       </Sheet>
 
-      <h1 className="text-lg font-semibold">
-        <span className="text-primary">FSW</span> Store
-      </h1>
+      <Link href={"/"}>
+        <h1 className="text-lg font-semibold">
+          <span className="text-primary">FSW</span> Store
+        </h1>
+      </Link>
 
       <Button size="icon" variant="outline">
         <ShoppingCartIcon />
