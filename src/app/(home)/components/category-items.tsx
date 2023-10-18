4370e9ce17c22c380966cd82e1d4932e4b9a3d5e
MarcoarCarleti <marcarleti@gmail.com>
2023-10-18 01:00:01 -0300

    feat: add page to show category products

--- src/app/(home)/components/category-items.tsx
+++ src/app/(home)/components/category-items.tsx
@@ -1,28 +1,12 @@
 import { Badge } from "@/components/ui/badge";
+import { categoryIcon } from "@/constants/category-icon";
 import { Category } from "@prisma/client";
-import {
-  HeadphonesIcon,
-  KeyboardIcon,
-  MonitorIcon,
-  MouseIcon,
-  SpeakerIcon,
-  SquareIcon,
-} from "lucide-react";
 
 interface CategoryItemProps {
   category: Category;
 }
 
 export function CategoryItem({ category }: CategoryItemProps) {
-  const categoryIcon = {
-    mouses: <MouseIcon size={16} />,
-    keyboards: <KeyboardIcon size={16} />,
-    monitors: <MonitorIcon size={16} />,
-    headphones: <HeadphonesIcon size={16} />,
-    mousepads: <SquareIcon size={16} />,
-    speakers: <SpeakerIcon size={16} />,
-  };
-
   return (
     <Badge
       variant="outline"
---
+++ src/constants/category-icon.tsx
@@ -0,0 +1,17 @@
+import {
+  MouseIcon,
+  KeyboardIcon,
+  MonitorIcon,
+  HeadphonesIcon,
+  SquareIcon,
+  SpeakerIcon,
+} from "lucide-react";
+
+export const categoryIcon = {
+  mouses: <MouseIcon size={16} />,
+  keyboards: <KeyboardIcon size={16} />,
+  monitors: <MonitorIcon size={16} />,
+  headphones: <HeadphonesIcon size={16} />,
+  mousepads: <SquareIcon size={16} />,
+  speakers: <SpeakerIcon size={16} />,
+};
