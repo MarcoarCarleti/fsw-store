c83178410f95b81619e7ede451d68ed5643529e4
MarcoarCarleti <marcarleti@gmail.com>
2023-10-21 00:09:23 -0300

    fix: localStorage

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -41,7 +41,12 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
   );
 
   useEffect(() => {
-    localStorage.setItem("@fsw-store/cart-products", JSON.stringify(products));
+    if (typeof localStorage !== "undefined") {
+      localStorage.setItem(
+        "@fsw-store/cart-products",
+        JSON.stringify(products),
+      );
+    }
   }, [products]);
 
   // Total sem descontos
