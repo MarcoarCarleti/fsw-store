3714a0b7f5aa14dc81bc34af0be29aac56af4ef3
MarcoarCarleti <marcarleti@gmail.com>
2023-10-23 22:32:52 -0300

    feat: add createOrder server action

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -9,14 +9,26 @@ import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
 import { createCheckout } from "@/actions/checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { createOrder } from "@/actions/order";
+import { useSession } from "next-auth/react";
 
 const Cart = () => {
+  const { data } = useSession();
   const { products, subTotal, total, totalDiscount } = useContext(CartContext);
 
   const handleFinishPurchaseClick = async () => {
+    if (!data?.user) {
+      // TODO! redicionar para tela de login
+      return;
+    }
+
+    await createOrder(products, (data?.user as any).id);
+
     const checkout = await createCheckout(products);
 
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    // Adicionar pedido no banco
 
     stripe?.redirectToCheckout({
       sessionId: checkout.id,
