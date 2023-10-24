c6fd625f22b5db335c3789898c614fa705463f70
MarcoarCarleti <marcarleti@gmail.com>
2023-10-24 13:41:35 -0300

    fix: ensure user goes to order page on order success

--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -3,7 +3,10 @@
 import { CartProduct } from "@/providers/cart";
 import Stripe from "stripe";
 
-export const createCheckout = async (products: CartProduct[], orderId: string) => {
+export const createCheckout = async (
+  products: CartProduct[],
+  orderId: string,
+) => {
   // CRIAR CHECKOUT
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2023-10-16",
@@ -12,7 +15,7 @@ export const createCheckout = async (products: CartProduct[], orderId: string) =
   const checkout = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
-    success_url: process.env.HOST_URL,
+    success_url: process.env.CONFIRMED_URL,
     cancel_url: process.env.HOST_URL,
     metadata: {
       orderId,
