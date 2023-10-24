6f4c1ababdaa986faf263f94f95f6680b78aa817
MarcoarCarleti <marcarleti@gmail.com>
2023-10-24 00:14:45 -0300

    feat: add OrderProductItemComponent

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -17,7 +17,9 @@ async function OrderPage() {
       userId: (user as any).id,
     },
     include: {
-      orderProducts: true,
+      orderProducts: {
+        include: { product: true },
+      },
     },
   });
 
